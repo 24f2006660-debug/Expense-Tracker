1c3830b4b4578e069d79371b6b2a504421761742
Your Name <you@example.com>
2026-04-19 20:20:58 +0530

    switch to pg8000

--- app.py
+++ app.py
@@ -13,9 +13,9 @@ database_url = os.getenv('DATABASE_URL')
 
 if database_url:
     if database_url.startswith("postgres://"):
-        database_url = database_url.replace("postgres://", "postgresql+psycopg2://", 1)
+        database_url = database_url.replace("postgres://", "postgresql+pg8000://", 1)
     elif database_url.startswith("postgresql://"):
-        database_url = database_url.replace("postgresql://", "postgresql+psycopg2://", 1)
+        database_url = database_url.replace("postgresql://", "postgresql+pg8000://", 1)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
