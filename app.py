6091cf81c0b6778bda397deab27a7dff1d9d8787
Your Name <you@example.com>
2026-04-19 20:15:06 +0530

    fix db and port

--- app.py
+++ app.py
@@ -11,9 +11,13 @@ app = Flask(__name__)
 # DATABASE
 database_url = os.getenv('DATABASE_URL')
 
-if database_url and database_url.startswith('postgresql://'):
-    database_url = database_url.replace('postgresql://', 'postgresql+psycopg2://', 1)
-app.config['SQLALCHEMY_DATABASE_URI'] = database_url or 'sqlite:///expenses.db'  # Fallback to SQLite
+if database_url:
+    if database_url.startswith("postgres://"):
+        database_url = database_url.replace("postgres://", "postgresql+psycopg2://", 1)
+    elif database_url.startswith("postgresql://"):
+        database_url = database_url.replace("postgresql://", "postgresql+psycopg2://", 1)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
     "connect_args": {"sslmode": "require"}
@@ -289,6 +293,9 @@ def get_expenses():
 
 
 # ---------------- RUN ----------------
+import os
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
     
